e5047f500f850da8db3a6413e88af9ee8a724e5a
kjsong99 <mine7118@kpu.ac.kr>
2022-11-07 23:59:30 +0900

    <style>: Set Background Gradient background gradient 생성

--- Spotify-Front/Spotify-Front/ViewController.swift
+++ Spotify-Front/Spotify-Front/ViewController.swift
@@ -1,19 +1,31 @@
-//
-//  ViewController.swift
-//  Spotify-Front
-//
-//  Created by 송경진 on 2022/11/07.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    func setGradient(){
+        let gradient = CAGradientLayer()
+        gradient.colors = [
+            hexStringToUIColor(hex: "#957C4D").withAlphaComponent(0.7).cgColor,
+            hexStringToUIColor(hex: "#121212").cgColor,
+            hexStringToUIColor(hex: "#121212").cgColor,
+            hexStringToUIColor(hex: "#121212").cgColor,
+            hexStringToUIColor(hex: "#121212").cgColor,
+            hexStringToUIColor(hex: "#121212").cgColor,
+            hexStringToUIColor(hex: "#121212").cgColor,
+        ]
+        
+        gradient.type = .axial
+        gradient.startPoint = CGPoint(x: 0.1, y: 0.3)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.35)
+        gradient.locations = [0.02, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18]
+        gradient.transform = CATransform3DMakeAffineTransform(CGAffineTransform(rotationAngle: 0.7))
+        gradient.bounds = view.layer.bounds.insetBy(dx:-0.5 * view.layer.bounds.size.width, dy: -0.5 * view.layer.bounds.size.height)
+        gradient.position = view.center
+        view.layer.addSublayer(gradient)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setGradient()
+      
     }
-
-
+    
 }
-
